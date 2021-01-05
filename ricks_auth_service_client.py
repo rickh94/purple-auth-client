da2493ea9149d7f88bdcfb14e9855c469ed3d39a
Rick Henry <rickhenry@rickhenry.dev>
2021-01-05 00:06:15 -0500

    add creation, starting auth flow, submitting otp code
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- ricks_auth_service_client.py
+++ ricks_auth_service_client.py
@@ -1,3 +1,88 @@
+from typing import Dict
+
+import aiohttp
+
+
+class AuthClientError(Exception):
+    pass
+
+
+class AppNotFound(AuthClientError):
+    pass
+
+
+class ServerError(AuthClientError):
+    pass
+
+
+class ValidationError(AuthClientError):
+    pass
+
+
+class AuthenticationFailure(AuthClientError):
+    pass
+
+
+def _check_response(response):
+    if response.status == 200:
+        return
+    if response.status == 404:
+        raise AppNotFound
+    if response.status == 500:
+        raise ServerError
+    if response.status == 422:
+        raise ValidationError
+    if response.status == 401 or response.status == 403:
+        raise AuthenticationFailure
+
+
 class AuthClient:
-    def __init__(self, host):
-        self.host = host
+    def __init__(self, host: str, app_id: str):
+        """Create the auth client objects
+
+        :param host: hostname of the auth server without 'https://'
+        :param app_id: The unique ID of the app to authenticate against.
+        """
+        self.host = "https://" + host
+        self.app_id = app_id
+        self._public_key = None
+
+    async def authenticate(self, email) -> str:
+        """Initialize authentication flow for user email
+
+        :param email: user's email address
+        :return: Message from server
+        :raises AppNotFound: if the app id is invalid.
+        :raises ServerError: if something goes wrong on the server.
+        :raises ValidationError: if something is wrong with the request.
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                f"{self.host}/otp/request/{self.app_id}",
+                json={"email": email},
+            ) as response:
+                _check_response(response)
+                return await response.json()
+
+    async def submit_code(self, email: str, code) -> Dict[str, str]:
+        """Submit an authentication code and get a token back
+
+        :param email: User's email address.
+        :param code: Submitted one time password code.
+        :returns: Dict containing id_token
+        :raises AppNotFound: if the app id is invalid (cannot be found).
+        :raises ServerError: if something goes wrong on the server.
+        :raises ValidationError: if something is wrong with the request data.
+        :raises AuthenticationFailure: if the email code combination doesn't authenticate.
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                f"{self.host}/otp/confirm/{self.app_id}",
+                json={"email": email, "code": code},
+            ) as response:
+                _check_response(response)
+                data = await response.json()
+                try:
+                    return {"id_token": data["idToken"]}
+                except KeyError:
+                    raise AuthClientError("idToken was not in response")
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,6 +1,151 @@
-from ricks_auth_service_client import AuthClient
+import re
+from urllib.parse import quote_plus
+
+import pytest
+from aioresponses import aioresponses
+
+from ricks_auth_service_client import (
+    AuthClient,
+    AppNotFound,
+    ServerError,
+    ValidationError,
+    AuthenticationFailure,
+)
 
 
-def test_exists():
-    client = AuthClient("localhost")
-    assert client.host == "localhost"
+@pytest.fixture
+def auth_client():
+    return AuthClient("auth.example.com", "123456")
+
+
+@pytest.fixture
+def mock_aioresponse():
+    with aioresponses() as m:
+        yield m
+
+
+def test_create():
+    client = AuthClient("localhost", "12345")
+    assert client.host == "https://localhost"
+    assert client.app_id == "12345"
+    assert client._public_key is None
+
+
+@pytest.mark.asyncio
+async def test_authenticate_success(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/request/{auth_client.app_id}",
+        status=200,
+        payload="Response message from server",
+    )
+
+    output = await auth_client.authenticate(fake_email)
+    assert output == "Response message from server"
+
+    request_args = list(mock_aioresponse.requests.values())[0][0].kwargs
+    assert request_args["json"]["email"] == fake_email
+
+
+@pytest.mark.asyncio
+async def test_authenticate_not_found(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/request/{auth_client.app_id}",
+        status=404,
+    )
+    with pytest.raises(AppNotFound):
+        await auth_client.authenticate(fake_email)
+
+
+@pytest.mark.asyncio
+async def test_authenticate_server_error(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/request/{auth_client.app_id}",
+        status=500,
+    )
+    with pytest.raises(ServerError):
+        await auth_client.authenticate(fake_email)
+
+
+@pytest.mark.asyncio
+async def test_authenticate_validation_error(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/request/{auth_client.app_id}",
+        status=422,
+    )
+    with pytest.raises(ValidationError):
+        await auth_client.authenticate(fake_email)
+
+
+@pytest.mark.asyncio
+async def test_submit_code_success(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    test_code = "11111111"
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/confirm/{auth_client.app_id}",
+        status=200,
+        payload={"idToken": "fake-id-token"},
+    )
+
+    output = await auth_client.submit_code(fake_email, test_code)
+
+    assert output == {"id_token": "fake-id-token"}
+
+    request_args = list(mock_aioresponse.requests.values())[0][0].kwargs
+    assert request_args["json"]["email"] == fake_email
+    assert request_args["json"]["code"] == test_code
+
+
+@pytest.mark.asyncio
+async def test_submit_code_not_found(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    test_code = "11111111"
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/confirm/{auth_client.app_id}",
+        status=404,
+    )
+
+    with pytest.raises(AppNotFound):
+        await auth_client.submit_code(fake_email, test_code)
+
+
+@pytest.mark.asyncio
+async def test_submit_code_validation_error(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    test_code = "11111111"
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/confirm/{auth_client.app_id}",
+        status=422,
+    )
+
+    with pytest.raises(ValidationError):
+        await auth_client.submit_code(fake_email, test_code)
+
+
+@pytest.mark.asyncio
+async def test_submit_code_server_error(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    test_code = "11111111"
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/confirm/{auth_client.app_id}",
+        status=500,
+    )
+
+    with pytest.raises(ServerError):
+        await auth_client.submit_code(fake_email, test_code)
+
+
+@pytest.mark.asyncio
+async def test_submit_code_authentication_failed(mock_aioresponse, auth_client, faker):
+    fake_email = faker.email()
+    test_code = "11111111"
+    mock_aioresponse.post(
+        f"{auth_client.host}/otp/confirm/{auth_client.app_id}",
+        status=401,
+    )
+
+    with pytest.raises(AuthenticationFailure):
+        await auth_client.submit_code(fake_email, test_code)
